bf46c504a13b6eae7704c50bb9944de1d2c28480
Gopu Aakash <gopuaakash751@gmail.com>
2025-12-09 21:04:32 +0530

    fix:oauth

--- routers/auth_router.py
+++ routers/auth_router.py
@@ -91,7 +91,7 @@ oauth.register(
     client_id=GOOGLE_CLIENT_ID,
     client_secret=GOOGLE_CLIENT_SECRET,
     server_metadata_url="https://accounts.google.com/.well-known/openid-configuration",
-    client_kwargs={"scope": "openid email"},
+    client_kwargs={"scope": "openid email profile"},
 )
 
 @router.get("/auth/google")
