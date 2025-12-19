754e3e310578cf762ed69a314b6282b84a24ec46
Gopu Aakash <gopuaakash751@gmail.com>
2025-12-19 08:25:32 +0530

    fix:print

--- routers/auth_router.py
+++ routers/auth_router.py
@@ -266,7 +266,7 @@ async def signup(user_data: UserCreate, request: Request):
     hashed_password = hash_password(user_data.password)
     print("Hashed password:", hashed_password)
     # Create user
-    print(origin)
+    print("origin:",origin)
     if("https://hero-x-admin.vercel.app" in origin):
         user_id = db.users.insert(
             username=user_data.username,
